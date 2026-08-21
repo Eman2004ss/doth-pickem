c4f29c5aab140d92e57647cc0730f0cd410bede2
Ethan Hawes <156271983+Eman2004ss@users.noreply.github.com>
2026-08-21 10:35:55 -0700

    add NCAA and NFL teams

---
+++ utils/team_data.py
@@ -0,0 +1,218 @@
+NFL_DIVISIONS = {
+    "AFC East": [
+        "Buffalo Bills",
+        "Miami Dolphins",
+        "New England Patriots",
+        "New York Jets"
+    ],
+    "AFC North": [
+        "Baltimore Ravens",
+        "Cincinnati Bengals",
+        "Cleveland Browns",
+        "Pittsburgh Steelers"
+    ],
+    "AFC South": [
+        "Houston Texans",
+        "Indianapolis Colts",
+        "Jacksonville Jaguars",
+        "Tennessee Titans"
+    ],
+    "AFC West": [
+        "Denver Broncos",
+        "Kansas City Chiefs",
+        "Las Vegas Raiders",
+        "Los Angeles Chargers"
+    ],
+    "NFC East": [
+        "Dallas Cowboys",
+        "New York Giants",
+        "Philadelphia Eagles",
+        "Washington Commanders"
+    ],
+    "NFC North": [
+        "Chicago Bears",
+        "Detroit Lions",
+        "Green Bay Packers",
+        "Minnesota Vikings"
+    ],
+    "NFC South": [
+        "Atlanta Falcons",
+        "Carolina Panthers",
+        "New Orleans Saints",
+        "Tampa Bay Buccaneers"
+    ],
+    "NFC West": [
+        "Arizona Cardinals",
+        "Los Angeles Rams",
+        "San Francisco 49ers",
+        "Seattle Seahawks"
+    ]
+}
+
+"ACC": [
+    "Boston College Eagles",
+    "California Golden Bears",
+    "Clemson Tigers",
+    "Duke Blue Devils",
+    "Florida State Seminoles",
+    "Georgia Tech Yellow Jackets",
+    "Louisville Cardinals",
+    "Miami Hurricanes",
+    "NC State Wolfpack",
+    "North Carolina Tar Heels",
+    "Pittsburgh Panthers",
+    "SMU Mustangs",
+    "Stanford Cardinal",
+    "Syracuse Orange",
+    "Virginia Cavaliers",
+    "Virginia Tech Hokies",
+    "Wake Forest Demon Deacons"
+],
+
+"SEC": [
+    "Alabama Crimson Tide",
+    "Arkansas Razorbacks",
+    "Auburn Tigers",
+    "Florida Gators",
+    "Georgia Bulldogs",
+    "Kentucky Wildcats",
+    "LSU Tigers",
+    "Mississippi State Bulldogs",
+    "Missouri Tigers",
+    "Ole Miss Rebels",
+    "Oklahoma Sooners",
+    "South Carolina Gamecocks",
+    "Tennessee Volunteers",
+    "Texas Longhorns",
+    "Texas A&M Aggies",
+    "Vanderbilt Commodores"
+],
+
+"Big Ten": [
+    "Illinois Fighting Illini",
+    "Indiana Hoosiers",
+    "Iowa Hawkeyes",
+    "Maryland Terrapins",
+    "Michigan Wolverines",
+    "Michigan State Spartans",
+    "Minnesota Golden Gophers",
+    "Nebraska Cornhuskers",
+    "Northwestern Wildcats",
+    "Ohio State Buckeyes",
+    "Oregon Ducks",
+    "Penn State Nittany Lions",
+    "Purdue Boilermakers",
+    "Rutgers Scarlet Knights",
+    "UCLA Bruins",
+    "USC Trojans",
+    "Washington Huskies",
+    "Wisconsin Badgers"
+],
+
+"Big 12": [
+    "Arizona Wildcats",
+    "Arizona State Sun Devils",
+    "Baylor Bears",
+    "BYU Cougars",
+    "Cincinnati Bearcats",
+    "Colorado Buffaloes",
+    "Houston Cougars",
+    "Iowa State Cyclones",
+    "Kansas Jayhawks",
+    "Kansas State Wildcats",
+    "Oklahoma State Cowboys",
+    "TCU Horned Frogs",
+    "Texas Tech Red Raiders",
+    "UCF Knights",
+    "Utah Utes",
+    "West Virginia Mountaineers"
+],
+
+"AAC": [
+    "Army Black Knights",
+    "Charlotte 49ers",
+    "East Carolina Pirates",
+    "Florida Atlantic Owls",
+    "Memphis Tigers",
+    "Navy Midshipmen",
+    "North Texas Mean Green",
+    "Rice Owls",
+    "South Florida Bulls",
+    "Temple Owls",
+    "Tulane Green Wave",
+    "Tulsa Golden Hurricane",
+    "UAB Blazers",
+    "UTSA Roadrunners"
+],
+
+"Mountain West": [
+    "Air Force Falcons",
+    "Boise State Broncos",
+    "Colorado State Rams",
+    "Fresno State Bulldogs",
+    "Hawaii Rainbow Warriors",
+    "Nevada Wolf Pack",
+    "New Mexico Lobos",
+    "San Diego State Aztecs",
+    "San Jose State Spartans",
+    "UNLV Rebels",
+    "Utah State Aggies",
+    "Wyoming Cowboys"
+],
+
+"Sun Belt": [
+    "Appalachian State Mountaineers",
+    "Arkansas State Red Wolves",
+    "Coastal Carolina Chanticleers",
+    "Georgia Southern Eagles",
+    "Georgia State Panthers",
+    "James Madison Dukes",
+    "Louisiana Ragin' Cajuns",
+    "Marshall Thundering Herd",
+    "Old Dominion Monarchs",
+    "South Alabama Jaguars",
+    "Southern Miss Golden Eagles",
+    "Texas State Bobcats",
+    "Troy Trojans",
+    "UL Monroe Warhawks"
+],
+
+"MAC": [
+    "Akron Zips",
+    "Ball State Cardinals",
+    "Bowling Green Falcons",
+    "Buffalo Bulls",
+    "Central Michigan Chippewas",
+    "Eastern Michigan Eagles",
+    "Kent State Golden Flashes",
+    "Miami (OH) RedHawks",
+    "Northern Illinois Huskies",
+    "Ohio Bobcats",
+    "Toledo Rockets",
+    "Western Michigan Broncos"
+],
+
+"Conference USA": [
+    "Delaware Blue Hens",
+    "Florida International Panthers",
+    "Jacksonville State Gamecocks",
+    "Kennesaw State Owls",
+    "Liberty Flames",
+    "Louisiana Tech Bulldogs",
+    "Middle Tennessee Blue Raiders",
+    "Missouri State Bears",
+    "New Mexico State Aggies",
+    "Sam Houston Bearkats",
+    "UTEP Miners"
+],
+
+"Pac-12": [
+    "Oregon State Beavers",
+    "Washington State Cougars"
+],
+
+"Independent": [
+    "Notre Dame Fighting Irish",
+    "Connecticut Huskies"
+]
+}
